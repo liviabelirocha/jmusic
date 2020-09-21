8b187d8de6a6f48504730eb52d824457ea144a7a
Phillipe Queiroz <phgqm7@gmail.com>
2020-09-21 12:29:10 -0300

    Fix javafx execution bug with jfoenix

---
+++ src/main/java/controllers/AppController.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class AppController {
+
+  @FXML
+  private JFXButton playlistButton;
+  @FXML
+  private JFXButton musicButton;
+  @FXML
+  private JFXButton exitButton;
+
+  @FXML
+  private Pane musicsPane;
+  @FXML
+  private Pane myPlaylistPane;
+  @FXML
+  private Pane playlistPane;
+
+  @FXML
+  void handlePaneChange(ActionEvent event) {
+    Object source = event.getSource();
+
+    handleNavigation(source);
+  }
+
+  private void handleNavigation(Object source) {
+    if (source == playlistButton) {
+      playlistPane.toFront();
+    } else if (source == musicButton) {
+      musicsPane.toFront();
+    } else if (source == exitButton) {
+      System.exit(1);
+    } else {
+      musicsPane.toFront();
+    }
+  }
+
+}
