5e84a551fe0bd10a335cfbafdad1f8ed7b2c281d
danielreboucas <danielreboucasq@gmail.com>
2020-10-19 20:11:48 -0300

    Fixed delete a music and remove it from it respective playlist

--- jmusic-api/src/main/java/com/app/jmusic/servicesImpl/MusicServiceImpl.java
+++ jmusic-api/src/main/java/com/app/jmusic/servicesImpl/MusicServiceImpl.java
@@ -1,8 +1,11 @@
 package com.app.jmusic.servicesImpl;
 
 import com.app.jmusic.models.Music;
+import com.app.jmusic.models.Playlist;
 import com.app.jmusic.repositories.MusicRepository;
+import com.app.jmusic.repositories.PlaylistRepository;
 import com.app.jmusic.servicesApi.MusicService;
+import com.app.jmusic.servicesApi.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,9 @@ import java.util.Optional;
 public class MusicServiceImpl implements MusicService<String, Music> {
   @Autowired
   private MusicRepository musicRepository;
+
+  @Autowired
+  private PlaylistRepository playlistRepository;
 
   @Override
   public Music insertMusic(Music music) throws Exception {
@@ -29,12 +35,12 @@ public class MusicServiceImpl implements MusicService<String, Music> {
     }
 
     return music.get();
-  };
+  }
 
   @Override
   public List<Music> getMusic() throws Exception {
     return musicRepository.findAll();
-  };
+  }
 
   @Override
   public List<Music> getMusics(List<String> musicIds) throws Exception {
@@ -60,10 +66,19 @@ public class MusicServiceImpl implements MusicService<String, Music> {
       return null;
     }
 
+    List<Playlist> playlists = playlistRepository.findAll();
+    for (Playlist playlist:playlists) {
+      List<String> musics = playlist.getMusics();
+      if (musics.contains(musicId)){
+        musics.remove(musicId);
+        playlist.setMusics(musics);
+      }
+      playlistRepository.save(playlist);
+    }
+
     Music someMusic = music.get();
 
     musicRepository.deleteById(musicId);
     return someMusic;
-  };
-
+  }
 }
